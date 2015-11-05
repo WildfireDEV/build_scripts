ad5dacccead5af47a68c28e503039bd3852c69d5
YoshiShaPow <yoshipga@gmail.com>
2015-11-05 16:45:10 -0500

    cm-13.0: update tag
    Signed-off-by: YoshiShaPow <yoshipga@gmail.com>

--- release.sh
+++ release.sh
@@ -3,4 +3,4 @@
 # This releases my cm-13.0 builds
 #
 
-mv /home/paradox/cm/out/target/product/bacon/cm-13-* ../../../www/devs/paradox/cm-13.0/rom/
+mv /home/paradox/cm/out/target/product/bacon/cm-13.0-* ../../../www/devs/paradox/cm-13.0/rom/
