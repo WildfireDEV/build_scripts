702f23a603ee04e078db9ee132f59df2b0cd74a0
Aiden K <YoshiShaPow@users.noreply.github.com>
2015-11-05 09:22:01 -0800

    cm-13.0: 2015-11-05
    Signed-off-by: YoshiShaPow <yoshipga@gmail.com>

--- sync.sh
+++ sync.sh
@@ -5,10 +5,10 @@
 #
 
 cd ~/cm/device/oppo/common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_oppo_common refs/changes/54/111754/2 && git cherry-pick FETCH_HEAD
-cd ~/cm/device/oppo/common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_oppo_common refs/changes/37/114337/6 && git cherry-pick FETCH_HEAD
-cd ~/cm/device/oppo/common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_oppo_common refs/changes/38/114838/3 && git cherry-pick FETCH_HEAD
-cd ~/cm/vendor/cm && git fetch http://review.cyanogenmod.org/CyanogenMod/android_vendor_cm refs/changes/36/111336/26 && git cherry-pick FETCH_HEAD
+cd ~/cm/device/oppo/common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_oppo_common refs/changes/84/116484/1 && git cherry-pick FETCH_HEAD
+cd ~/cm/device/oppo/common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_oppo_common refs/changes/94/117294/1 && git cherry-pick FETCH_HEAD
+cd ~/cm/vendor/cm && git fetch http://review.cyanogenmod.org/CyanogenMod/android_vendor_cm refs/changes/36/111336/28 && git cherry-pick FETCH_HEAD
 cd ~/cm/vendor/cm && git fetch http://review.cyanogenmod.org/CyanogenMod/android_vendor_cm refs/changes/51/116051/2 && git cherry-pick FETCH_HEAD
-cd ~/cm/frameworks/base && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/94/114594/1 && git cherry-pick FETCH_HEAD
-cd ~/cm/packages/apps/Settings && git fetch http://review.cyanogenmod.org/CyanogenMod/android_packages_apps_Settings refs/changes/79/116079/1 && git cherry-pick FETCH_HEAD
+cd ~/cm/device/oneplus/bacon && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_oneplus_bacon refs/changes/00/117700/2 && git cherry-pick FETCH_HEAD
+cd ~/cm/device/oneplus/bacon && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_oneplus_bacon refs/changes/88/117688/1 && git cherry-pick FETCH_HEAD
 cd ~/cm
