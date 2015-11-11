79ca80eb2939457f092af57ef3330abae35e0e41
WildfireDEV <wildfiredev@gmail.com>
2015-11-11 17:53:32 +1100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -15,18 +15,8 @@ cd ~/CM13/device/htc/m7-common && git fetch http://review.cyanogenmod.org/Cyanog
 # MSM8960-common
 cd ~/CM13/device/msm8960-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_msm8960-common refs/changes/30/113530/3 && git cherry-pick FETCH_HEAD
 
-# CM vendor
-cd ~/CM13/vendor/cm && git fetch http://review.cyanogenmod.org/CyanogenMod/android_vendor_cm refs/changes/36/111336/30 && git cherry-pick FETCH_HEAD
-
 # LibHardware Legacy
 cd ~/CM13/hardware/libhardware_legacy && git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_libhardware_legacy refs/changes/29/113229/3 && git cherry-pick FETCH_HEAD
-
-# Frameworks (AV)
-cd ~/CM13/frameworks/av && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/68/117868/1 && git cherry-pick FETCH_HEAD
-cd ~/CM13/frameworks/av && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/69/117869/1 && git cherry-pick FETCH_HEAD
-cd ~/CM13/frameworks/av && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/70/117870/1 && git cherry-pick FETCH_HEAD
-cd ~/CM13/frameworks/av && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/71/117871/1 && git cherry-pick FETCH_HEAD
-cd ~/CM13/frameworks/av && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/18/118018/1 && git cherry-pick FETCH_HEAD
 
 # Build
 cd ~/CM13/build && git fetch http://review.cyanogenmod.org/CyanogenMod/android_build refs/changes/73/117873/2 && git cherry-pick FETCH_HEAD
@@ -48,7 +38,7 @@ cd ~/CM13/hardware/qcom/display-caf/msm8960 && git fetch http://review.cyanogenm
 cd ~/CM13/frameworks/opt/telephony && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/05/114805/3 && git cherry-pick FETCH_HEAD
 cd ~/CM13/frameworks/opt/telephony && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/03/114803/3 && git cherry-pick FETCH_HEAD
 cd ~/CM13/frameworks/opt/telephony && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/06/114806/3 && git cherry-pick FETCH_HEAD
-#cd ~/CM13/frameworks/opt/telephony && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/04/114804/4 && git cherry-pick FETCH_HEAD
+cd ~/CM13/frameworks/opt/telephony && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/04/114804/4 && git cherry-pick FETCH_HEAD
 cd ~/CM13/frameworks/opt/telephony && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/81/114381/1 && git cherry-pick FETCH_HEAD
 cd ~/CM13/frameworks/opt/telephony && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/31/117231/1 && git cherry-pick FETCH_HEAD
 cd ~/CM13/frameworks/opt/telephony && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/67/114867/1 && git cherry-pick FETCH_HEAD
@@ -56,4 +46,7 @@ cd ~/CM13/frameworks/opt/telephony && git fetch http://review.cyanogenmod.org/Cy
 cd ~/CM13/frameworks/opt/telephony && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/24/117924/1 && git cherry-pick FETCH_HEAD
 cd ~/CM13/frameworks/opt/telephony && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/26/118026/1 && git cherry-pick FETCH_HEAD
 cd ~/CM13/frameworks/opt/telephony && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_opt_telephony refs/changes/72/114872/4 && git cherry-pick FETCH_HEAD
+
+# Art Runtime
+cd ~/CM13/art && git fetch http://review.cyanogenmod.org/CyanogenMod/android_art refs/changes/99/117799/2 && git cherry-pick FETCH_HEAD
 cd ~
